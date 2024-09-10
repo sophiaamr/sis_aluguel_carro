38509915997202471a9a6cca1dd1499abcebd546
Ana Cândido <128816273+anajuliateixeiracandido@users.noreply.github.com>
2024-09-10 19:38:14 -0300

    inicialização

---
+++ src/SistemaAluguel/src/test/java/com/example/SistemaAluguel/SistemaAluguelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SistemaAluguel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAluguelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
